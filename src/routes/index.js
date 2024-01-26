829b3fe10be7ac16c2d00ddfdb0bb02bd569cb8e
d.filyukov <d.filyukov@lucky-team.pro>
2024-01-26 17:36:10 +0100

    Added root endpoint handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import postPatreonToken from '../services/patreon.js';
 
 async function routes(fastify) {
+  fastify.get('/', async (req, reply) => ({ hello: 'world' }));
+
   fastify.get('/patreon/oauth', async (req, reply) => {
     const { state, code } = req.query;
     const patreonToken = postPatreonToken(code);
