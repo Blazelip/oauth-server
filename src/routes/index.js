fb0e662f84b136fc61799a98b07a074eaaec1224
d.filyukov <d.filyukov@lucky-team.pro>
2024-02-21 22:31:25 +0100

    Added debug logs2

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ async function routes(fastify) {
 
   fastify.get('/patreon/oauth', async (req) => {
     const { state, code } = req.query;
+    console.log("🚀 ~ fastify.get ~ state:", state)
     console.log("🚀 ~ fastify.get ~ code:", code)
     const {
       access_token: accessToken,
