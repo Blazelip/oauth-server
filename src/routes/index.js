9196d3ff6711972cc34bebc4e44f5dd1c4dd3083
d.filyukov <d.filyukov@lucky-team.pro>
2024-02-21 22:06:01 +0100

    Added some logs in PatreonTOken

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,8 +17,6 @@ async function routes(fastify) {
       tokenExpiration,
       refreshToken,
     });
-
-    // {"access_token": "2Usre1r6M_ELi4oyd41athWJ9rvtbXH35tbDBg2Z070", "expires_in": 2678400, "token_type": "Bearer", "scope": "identity w:campaigns.benefits campaigns.members campaigns.members.address campaigns.members[email] campaigns.posts w:campaigns.posts w:campaigns.webhook campaigns.webhook campaigns w:campaigns.apps apps.tiers identity[email] identity.memberships w:identity.clients", "refresh_token": "jJ6ZZp0Hb3PwQ1nc2BniOfvO7WtH1WKA0nLHl5Uw6sg", "version": "0.0.1"}
   });
 }
 
